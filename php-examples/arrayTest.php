0d061a3227caf93c10b731da4aaa4d3e5dcb3782
paul rene nichols <paul.rene.nichols@gmail.com>
2013-02-04 11:52:12 -0600

    Adding simple php examples

---
+++ php-examples/arrayTest.php
@@ -0,0 +1,6 @@
+<?php
+    $names = array("Lao Tzu", "Sun Wu", "Cao Cao", "Ingmar Bergman");
+    echo $names;
+    echo "<br />";
+    echo $names[3];  //Arrays appear to be 0-indexed in php, just like C, C++, and JavaScript
+?>
---
+++ php-examples/username.php
@@ -0,0 +1,8 @@
+<?php
+	$username = "Mister Nichols";
+    echo $username;
+    echo "<br />";
+    $current_user = $username;
+    echo $current_user;
+?>
+
